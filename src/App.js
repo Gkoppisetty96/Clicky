fefbe25872d52771e8a92794a3573a32e9aad357
Ithaca <gkoppisetty96@gmail.com>
2019-06-30 23:49:13 -0400

    V 1.0 working app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
 import React, { Component } from "react";
 import Card from "./components/Card";
 import Wrapper from "./components/Wrapper";
